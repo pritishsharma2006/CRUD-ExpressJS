6de5d46f26632ee40b7ae2190b709f0f6bb73d7c
Pritish Kumar Sharma <sharmapritishkumar@gmail.com>
2026-04-07 18:25:40 +0530

    add dotenv dependency for environment variable management

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+import 'dotenv/config'
 import express from 'express'
 
 const app = express()
-const port = 3000
+const port =  process.env.PORT|| 3000
 app.use(express.json()) // So that Express can read Json data and convert it as JS object
 
 let teaData = []
