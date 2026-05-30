ea88e4e0799960ea5671e33b076e08b93750bbf2
Pritish Kumar Sharma <sharmapritishkumar@gmail.com>
2026-05-30 11:53:28 +0530

    Advanced Logging with morgan and winston

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 import 'dotenv/config'
 import express from 'express'
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express()
 const port =  process.env.PORT|| 3000
 app.use(express.json()) // So that Express can read Json data and convert it as JS object
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let teaData = []
 let nextId = 1
 
 // add a new Tea !!
 app.post('/teas',(req,res)=>{
+    logger.warn('Adding a new tea')
     const {name,price} = req.body
     const newTea = {id:nextId++,name,price}
     teaData.push(newTea)
